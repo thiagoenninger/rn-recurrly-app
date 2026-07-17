2ed9bc78376a560c377ae0649a9fae4a91a5933b
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2026-07-17 14:49:58 -0300

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import "@/global.css";
 import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
@@ -8,40 +9,32 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link
-        href={"/onboarding"}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Go to Onboarding
-      </Link>
+    <>
+      <StatusBar style="dark" />
+      <SafeAreaView className="flex-1 bg-background p-5">
+        <Text className="text-5xl font-sans-bold">Home</Text>
 
-      <Link
-        href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Go to Sign In
-      </Link>
+        <Link
+          href={"/onboarding"}
+          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+        >
+          Go to Onboarding
+        </Link>
 
-      <Link
-        href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Go to Sign Up
-      </Link>
+        <Link
+          href={"/(auth)/sign-in"}
+          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+        >
+          Go to Sign In
+        </Link>
 
-      <Link href="/subscriptions/spotify"> Spotify Subscription</Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" },
-        }}
-      >
-        Claude Pro Subscription
-      </Link>
-    </SafeAreaView>
+        <Link
+          href={"/(auth)/sign-up"}
+          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+        >
+          Go to Sign Up
+        </Link>
+      </SafeAreaView>
+    </>
   );
 }
